9078af11a0e7ecd79a934a5c87215797c2feed9b
Abdiel Cortés <abdiel.cortes@upr.edu>
2022-11-27 17:03:48 -0400

    Temporarily show screen name and user name in profile (#27)

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Typography } from "@mui/material";
+import AuthContext from "../../context/AuthProvider";
 
 const Profile = () => {
+  const { session } = useContext(AuthContext);
   return (
     <React.Fragment>
-      <Typography>I am profile</Typography>
+      <Typography>
+        I am {session.user.screenname} @{session.user.username}
+      </Typography>
     </React.Fragment>
   );
 };
